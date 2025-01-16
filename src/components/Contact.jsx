15db78b0b72ccf48b03be155f2b53f163a3f1c7d
hitesh989998 <hitesh989998@gmail.com>
2025-01-17 02:53:10 +0530

    completed experience component + misc ui fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,31 +1,34 @@
 import { FaGithub } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <main className="flex w-full h-[770px] gap-0 mb-8 p-0 rounded-3xl">
-      {/* Section 1 */}
-      <section className="relative flex flex-col w-[10%] h-full bg-white p-4 items-center rounded-l-3xl">
-        {/* Extended Background */}
+      <section className="relative flex flex-col w-[15%] h-full bg-white p-4 items-center rounded-l-3xl">
         <div className="absolute inset-y-0 right-[-20px] bg-white w-[20px] z-10"></div>
-        <div className="flex flex-col justify-between h-full rounded-3xl">
-          <p className="text-lg text-[#1e3a8a]">Text1</p>
-          <p className="text-xs text-[#1e3a8a]">Text2</p>
+        <div className="flex flex-col justify-between items-center h-full rounded-3xl">
+          <p className="text-lg text-[#1e3a8a]">Portfolio</p>
+          <p className="text-lg font-extrabold font-Pinyon tracking-widest text-neutral-300 top-10 absolute">
+            Hitesh Lalwani
+          </p>
+          <p className="text-xs text-neutral-300">© 2025</p>
         </div>
       </section>
 
-      {/* Section 2 */}
       <section className="flex flex-col w-[45%] h-full bg-blue-50 p-4 rounded-l-3xl z-20 relative">
         <div className="flex flex-col justify-between h-full">
-          <p className="text-4xl text-[#1e3a8a]">Text1</p>
-          <p className="text-2xl text-[#1e3a8a]">Text2</p>
+          <p className="text-6xl text-[#1e3a8a]">
+            A good coder writes code for the computer.
+          </p>
+          <p className="text-2xl text-[#1e3a8a]">
+            a great coder writes code for humans.
+          </p>
         </div>
         <div className="absolute inset-y-0 right-[-20px] bg-blue-50 w-[40px] z-10"></div>
       </section>
 
-      {/* Section 3 */}
       <section className="flex flex-col w-[45%] h-full rounded-r-3xl p-6 pb-0 z-30 bg-dark_primary rounded-3xl relative">
-        {/* Contact Info */}
         <div className="flex flex-col w-full gap-4 text-white">
           <h2 className="text-3xl font-bold text-white">Contact Information</h2>
           <div className="flex justify-start gap-4">
@@ -40,9 +43,15 @@ export default function Contact() {
               </span>
             </div>
             <a
-              href="https://github.com"
+              href="https://www.linkedin.com/in/hitesh-lalwani989998/"
               target="_blank"
-              rel="noopener noreferrer"
+              className="flex items-center justify-center w-10 h-10 rounded-full shadow-lg text-white shadow-dark transition-all duration-300 shadow-hover2"
+            >
+              <FaLinkedin size={24} />
+            </a>
+            <a
+              href="https://github.com/hitesh989998"
+              target="_blank"
               className="flex items-center justify-center w-10 h-10 rounded-full shadow-lg text-white shadow-dark transition-all duration-300 shadow-hover2"
             >
               <FaGithub size={24} />
@@ -50,7 +59,6 @@ export default function Contact() {
           </div>
         </div>
 
-        {/* Download My CV Section */}
         <div className="mt-4 mb-28">
           <a
             href="/path-to-cv.pdf"
@@ -63,44 +71,37 @@ export default function Contact() {
             </span>
           </a>
         </div>
-        <h2 className="text-white top-72 right-5 font-Pinyon absolute z-10 whitespace-nowrap text-4xl tracking-widest">
+        <h2 className="text-white top-72 right-1 p-1 font-Pinyon absolute z-10 whitespace-nowrap text-4xl tracking-widest">
           Let’s build something great together.{" "}
         </h2>
 
-        {/* Contact Form */}
         <div className="flex-grow mt-4 flex w-full">
-          {/* Contact Form Text */}
           <div className="w-[10%] flex justify-center items-center">
             <h2 className="text-5xl text-dark_primary transform -rotate-90 origin-center whitespace-nowrap  shadow-dark-text font-Poppins">
               Contact Form
             </h2>
           </div>
 
-          {/* Form */}
           <div className="w-[90%] flex justify-center items-center">
             <div className="w-full h-full p-2 bg-dark_primary backdrop-blur-sm rounded-3xl flex justify-center items-center">
               <form className="w-full space-y-6">
-                {/* Name Input */}
                 <input
                   type="text"
                   placeholder="Your Name"
                   className="block w-full p-4 bg-transparent text-white rounded-lg shadow-form-input"
                 />
 
-                {/* Email Input */}
                 <input
                   type="email"
                   placeholder="Your Email"
                   className="block w-full p-4 bg-transparent text-white rounded-lg shadow-form-input"
                 />
 
-                {/* Message Textarea */}
                 <textarea
                   placeholder="Your Message"
                   className="block w-full p-4 bg-transparent text-white rounded-lg shadow-form-input h-32"
                 />
 
-                {/* Submit Button */}
                 <button
                   type="submit"
                   className="w-full py-3 shadow-dark text-white rounded-full text-lg font-semibold shadow-hover2 transition-all duration-300"
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,18 +1,21 @@
 export default function Experience() {
   return (
-    <main className="flex w-full h-[770px] gap-4 mb-8 p-4 bg-white rounded-3xl">
+    <main className="flex w-full h-[770px] gap-2 mb-8 p-4 bg-blue-50 rounded-3xl">
       {/* Section: About Me */}
-      <section
-        className="flex-1 bg-cover bg-center relative rounded-3xl overflow-hidden hover:scale-105 transition-transform duration-300"
-        style={{ backgroundImage: "url('./starry.png')" }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50 transition-opacity duration-300"></div>
-        <div className="relative z-10 bg-white bg-opacity-20 backdrop-blur-lg p-6 rounded-3xl h-full flex flex-col justify-start">
+      <section className="flex-1 bg-cover bg-center relative rounded-3xl overflow-hidden bg-[url('./glass4.webp')]">
+        <div className="absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50"></div>
+        <div className="relative z-10 p-6 rounded-3xl h-full flex flex-col justify-start">
           <h2 className="text-3xl font-bold text-white mb-4">About Me</h2>
           <p className="text-sm text-white text-left">
-            I am a passionate individual with a drive for continuous learning
-            and growth. I thrive in environments that encourage innovation and
-            collaboration.
+            I’m someone who thrives on upskilling, constantly learning, and
+            pushing my skills to the next level. With a near-perfect mastery of
+            the MERN stack, I specialize in building scalable, high-performance
+            web apps that deliver a real impact.
+            <br /> <br />
+            Making the shift from Digital Marketing to tech was a transformative
+            step, allowing me to embrace the challenges of coding and continuous
+            learning—a journey perfectly aligned with my strengths and future
+            goals.
           </p>
         </div>
         <img
@@ -22,25 +25,24 @@ export default function Experience() {
         />
       </section>
 
-      {/* Section: Experience */}
-      <section
-        className="flex-1 bg-cover bg-center relative rounded-3xl overflow-hidden hover:scale-105 transition-transform duration-300"
-        style={{ backgroundImage: "url('./starry.png')" }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50 transition-opacity duration-300"></div>
-        <div className="relative z-10 bg-white bg-opacity-20 backdrop-blur-lg p-6 rounded-3xl h-full flex flex-col justify-start">
+      <section className="flex-1 bg-cover bg-center relative rounded-3xl overflow-hidden bg-[url('./glass2.jpg')]">
+        <div className="absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50"></div>
+        <div className="relative z-10 bg-white bg-opacity-30 p-6 rounded-3xl h-full flex flex-col justify-start">
           <h2 className="text-3xl font-bold text-white mb-4">Experience</h2>
 
           {/* Role 1 */}
           <div className="mb-4">
             <div className="flex justify-between items-center text-white">
-              <h3 className="text-xl font-semibold">Software Engineer</h3>
-              <span className="text-sm italic">Company 1 - Present</span>
+              <h3 className="text-lg font-semibold">MERN Stack Intern</h3>
+              <span className="text-sm">August 2024 - Present</span>
             </div>
-            <h4 className="text-xs">sample</h4>
+            <h4 className="text-xs">Kangaroo Software Pvt. Ltd.</h4>
             <p className="text-sm text-white">
-              Developed scalable web applications using modern frameworks and
-              ensured seamless user experiences.
+              During my MERN Stack internship, I worked on real-world projects
+              using React, Node.js, and MongoDB. I focused on deploying
+              applications and writing production-level code to ensure
+              scalability and performance. I collaborated with my team to debug,
+              optimize, and deliver reliable solutions for live environments.
             </p>
             <hr className="border-t border-gray-400 my-2" />
           </div>
@@ -48,13 +50,16 @@ export default function Experience() {
           {/* Role 2 */}
           <div className="mb-4">
             <div className="flex justify-between items-center text-white">
-              <h3 className="text-xl font-semibold">Frontend Developer</h3>
-              <span className="text-sm italic">Company 2 - Present</span>
+              <h3 className="text-lg font-semibold">
+                Digital Marketing Executive
+              </h3>
+              <span className="text-sm italic">March 2024 - July 2024</span>
             </div>
-            <h4 className="text-xs">sample</h4>
+            <h4 className="text-xs">Rashail Tech Labs</h4>
             <p className="text-sm text-white">
-              Designed responsive user interfaces and collaborated with
-              cross-functional teams to achieve project goals.
+              Responsible for growing IG account 4X and Pinterest 10x in 5
+              months. Received laurels for reaching 3 lakh video plays in 3 days
+              on a viral IG post.
             </p>
             <hr className="border-t border-gray-400 my-2" />
           </div>
@@ -62,13 +67,21 @@ export default function Experience() {
           {/* Role 3 */}
           <div>
             <div className="flex justify-between items-center text-white">
-              <h3 className="text-xl font-semibold">UI/UX Designer</h3>
-              <span className="text-sm italic">Company 3 - Present</span>
+              <h3 className="text-xl font-semibold">
+                Digital Marketing Intern
+              </h3>
+              <span className="text-sm">July 2023 - Jan 2024</span>
             </div>
-            <h4 className="text-xs">sample</h4>
+            <h4 className="text-xs">
+              DigitalVia Technologies Pvt. Ltd (Formerly SG Gurukul)
+            </h4>
             <p className="text-sm text-white">
-              Created visually appealing designs that improved user engagement
-              and customer satisfaction.
+              Focused on SEO, I conducted keyword and competitor research,
+              optimized meta tags, and improved content for better ranking.
+              Created and submitted Web 2.0 properties, managed link-building,
+              and optimized Google My Business listings. Excelled in content
+              creation, social media marketing, and built custom WordPress
+              websites using Elementor and Astra.
             </p>
             <hr className="border-t border-gray-400 my-2" />
           </div>
@@ -80,41 +93,37 @@ export default function Experience() {
         />
       </section>
 
-      {/* Section: Education */}
-      <section
-        className="flex-1 bg-cover bg-center relative rounded-3xl overflow-hidden hover:scale-105 transition-transform duration-300"
-        style={{ backgroundImage: "url('./starry.png')" }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-30 hover:bg-opacity-50 transition-opacity duration-300"></div>
-        <div className="relative z-10 bg-white bg-opacity-20 backdrop-blur-lg p-6 rounded-3xl h-full flex flex-col justify-start">
+      <section className="flex-1 bg-cover bg-center relative rounded-3xl overflow-hidden bg-[url('./glassk.jpg')]">
+        <div className="absolute inset-0 bg-black bg-opacity-20 hover:bg-opacity-50"></div>
+        <div className="relative z-10 bg-transparent bg-opacity-80 p-6 rounded-3xl h-full flex flex-col justify-start">
           <h2 className="text-3xl font-bold text-white mb-4">Education</h2>
 
-          {/* MBA */}
           <div className="mb-2">
             <div className="flex justify-between items-center text-white">
-              <h3 className="text-xl font-semibold">MBA (IT + Marketing)</h3>
-              <span className="text-sm italic">
-                XYZ Business School - Present
-              </span>
+              <h3 className=" text-base whitespace-nowrap text-white font-semibold underline">
+                Dr. A.P.J Abdul Kalam University
+              </h3>
+              <span className="text-sm  text-white">2021 - 2023</span>
             </div>
-            <h4 className="text-xs">sample</h4>
+            <h4 className="text-xs text-white">MBA, Marketing + IT </h4>
             <p className="text-sm text-white">
-              Gained insights into IT strategies and marketing principles to
-              drive business growth and improve organizational effectiveness.
+              Master of Business Administration (MBA) with dual specialization
+              in Marketing and Information Systems Management (IT).
             </p>
           </div>
 
-          {/* BBA */}
           <div>
             <div className="flex justify-between items-center text-white">
-              <h3 className="text-xl font-semibold">BBA (IT + Marketing)</h3>
-              <span className="text-sm italic">ABC University - Present</span>
+              <h3 className="text-base whitespace-nowrap text-white font-semibold underline mt-5">
+                {" "}
+                Dr. A.P.J Abdul Kalam University
+              </h3>
+              <span className="text-sm  text-white mt-5">2018-2021</span>
             </div>
-            <h4 className="text-xs">sample</h4>
+            <h4 className="text-xs text-white">BBA, Marketing</h4>
             <p className="text-sm text-white">
-              Built foundational knowledge in IT and marketing through hands-on
-              projects and coursework that sharpened leadership and teamwork
-              skills.
+              Bachelor of Business Administration (BBA) with a specialization in
+              Marketing, focusing on Business Administration and Management.
             </p>
           </div>
         </div>
